13c14381f910cfd2e36f2619e9c77113efcab02c
HP <HP@DESKTOP-GQMK5Q9>
2019-06-27 00:49:07 +0100

    rpDirecteur01

--- e-FormaPro v2.0/Forms/Directeur/ProfileDericteur.aspx.cs
+++ e-FormaPro v2.0/Forms/Directeur/ProfileDericteur.aspx.cs
@@ -4,8 +4,6 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using System.Data;
-using System.Data.SqlClient;
 
 
 
@@ -25,65 +23,6 @@ namespace e_FormaPro_v2._0.Forms.Directeur
             //        mv.ActiveViewIndex = 1;
             //    }
             //}
-            getUrlNam();
-
-        }
-
-
-        protected void Button1_Click(object sender, EventArgs e)
-        {
-            string patern;
-
-            FileUpload1.SaveAs(Server.MapPath("~/img/Directeur" + FileUpload1.FileName));
-
-            patern = "~/img/Directeur" + FileUpload1.FileName;
-
-
-
-            imgPicture.ImageUrl = patern;
-
-
-
-            SqlConnection Connection = new SqlConnection(@"Data Source=DESKTOP-GQMK5Q9\SQLEXPRESS;Initial Catalog=Espace_pédagogique2;Integrated Security=True");
-            SqlCommand command = new SqlCommand();
-
-            string matricule = "M01";
-            //    string papa = "aaaa";
-
-
-            command.CommandText = string.Format(@"  update Directeurs set Urlname = '{0}'
-                                                    where Matricule = '{1}' ", patern.ToString(), matricule);
-
-            //cmd.Parameters.AddWithValue("@query_name", txt_query_name.Text);
-
-            command.Connection = Connection;
-
-            Connection.Open();
-            command.ExecuteNonQuery();
-            Connection.Close();
-
-            //getUrlNam();
-        }
-
-        public void getUrlNam()
-        {
-            SqlConnection Connection = new SqlConnection(@"Data Source=DESKTOP-GQMK5Q9\SQLEXPRESS;Initial Catalog=Espace_pédagogique2;Integrated Security=True");
-            SqlCommand command = new SqlCommand();
-
-            string matricule = "M01";
-            string urlname;
-
-            command.CommandText = string.Format(@"select  Urlname from Directeurs
-                                                  where Matricule = '{0}' ", matricule);
-
-
-
-            command.Connection = Connection;
-
-            Connection.Open();
-            urlname = command.ExecuteScalar().ToString();
-            Connection.Close();
-            imgPicture.ImageUrl = urlname;
         }
     }
 }
